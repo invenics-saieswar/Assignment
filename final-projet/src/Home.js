918299f64db636af902099f99c78d544082ad2f3
invenics-rishikesh <rishikesh.singh@invenics.com>
2023-12-18 20:53:53 +0530

    new branch

---
+++ final-projet/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <h1>This is the home page content.</h1>
+      <a href="/App">Back</a>
+    </div>
+  );
+  
+};
+
+export default HomePage;
